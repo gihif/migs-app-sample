c3396b08ef0af41f37835fc798a08431b916642a
Arif Ginanjar <arif.ginanjar@agate.id>
2020-06-10 08:51:12 +0700

    fixing zone and template value swapped

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,10 +1,10 @@
 class HomeController < ApplicationController
   def index
     cmd = "gcloud compute instance-groups managed list-instances #{ENV['GCLOUD_MIGS_NAME']} --region #{ENV['GCLOUD_MIGS_REGION']} --filter=\"NAME:$(hostname)\" --format=\"table(INSTANCE_TEMPLATE)\""
-    @zone = `#{cmd}`.split("\n").last rescue 'command not found'
+    @template = `#{cmd}`.split("\n").last rescue 'command not found'
 
     cmd = "gcloud compute instance-groups managed list-instances #{ENV['GCLOUD_MIGS_NAME']} --region #{ENV['GCLOUD_MIGS_REGION']} --filter=\"NAME:$(hostname)\" --format=\"table(ZONE)\""
-    @template = `#{cmd}`.split("\n").last rescue 'command not found'
+    @zone = `#{cmd}`.split("\n").last rescue 'command not found'
 
     @hostname = %x{ hostname }
     @version = ''
