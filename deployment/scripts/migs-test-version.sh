3bdbc95a671269aab114aabc6da600b2d22adacf
Arif Ginanjar <arif.ginanjar@agate.id>
2020-06-11 06:10:58 +0700

    try to fixing looping proses

--- deployment/scripts/migs-test-version.sh
+++ deployment/scripts/migs-test-version.sh
@@ -6,9 +6,9 @@ PROJECT_URL_API='/version'
 set -e
 
 echo "URL Destination: ${PROJECT_URL_PROTOCOL}${PROJECT_URL_DOMAIN}${PROJECT_URL_API}"
-for i in {1..300}
+for ((i=1;i<=300;i++))
 do
-  curl ${PROJECT_URL_PROTOCOL}${PROJECT_URL_DOMAIN}${PROJECT_URL_API}
+  curl -v --header "Connection: keep-alive" "${PROJECT_URL_PROTOCOL}${PROJECT_URL_DOMAIN}${PROJECT_URL_API}"
   sleep 2
-  $i
+  echo " "
 done
