46cab38b6fd015633ae357d1430338ebc24c1ac3
Arif Ginanjar <arif.ginanjar@agate.id>
2020-06-11 06:21:41 +0700

    still error with looping. trying fixing again using while

--- deployment/scripts/migs-test-zone.sh
+++ deployment/scripts/migs-test-zone.sh
@@ -6,9 +6,12 @@ PROJECT_URL_API='/zone'
 set -e
 
 echo "URL Destination: ${PROJECT_URL_PROTOCOL}${PROJECT_URL_DOMAIN}${PROJECT_URL_API}"
-for ((i=1;i<=300;i++))
-do
+
+i=0
+while [ "$i" -le 300 ]; do
   curl -v --header "Connection: keep-alive" "${PROJECT_URL_PROTOCOL}${PROJECT_URL_DOMAIN}${PROJECT_URL_API}"
   sleep 2
   echo " "
+
+  i=$(( i + 1 ))
 done
