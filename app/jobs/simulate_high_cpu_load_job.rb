b23c6583ae2358da4bc1692d1bb80ba69d5f61ee
Rifqi Fauzi <rifqi.thefi.fauzi@gmail.com>
2020-06-11 09:45:39 +0700

    increase spawn workers

--- app/jobs/simulate_high_cpu_load_job.rb
+++ app/jobs/simulate_high_cpu_load_job.rb
@@ -2,7 +2,7 @@ class SimulateHighCpuLoadJob < ApplicationJob
   queue_as :default
 
   def perform
-    cmd = '/usr/local/bin/stress --cpu 8 --timeout 120'
+    cmd = '/usr/local/bin/stress --cpu 64 --timeout 120'
     puts `#{cmd}`
   end
 end
